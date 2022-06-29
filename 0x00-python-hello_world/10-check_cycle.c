895491eca648fd4528cb23c4f8bc21026c8bf098
kenkomu <komukenneth216@gmail.com>
2022-06-29 19:21:31 +0000

    checks if a singly linked list has a cycle in it

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,63 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
+ * check_cycle - check if a linked list has a loop
+ *
+ * @list: first node
+ *
+ * Return: 1: if linked list have a loop
+ *         0: if not
  */
-size_t print_listint(const listint_t *h)
+int check_cycle(listint_t *list)
 {
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
+	listint_t *sHead, *fHead;
 
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
-    }
+	sHead = fHead = list;
 
-    return (n);
-}
+	if (list == NULL)
+		return (0);
 
-/**
- * add_nodeint - adds a new node at the beginning of a listint_t list
- * @head: pointer to a pointer of the start of the list
- * @n: integer to be included in node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-    listint_t *new;
+	while (sHead && fHead && fHead->next)
+	{
+		sHead = sHead->next;
+		fHead = fHead->next->next;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
+		if (sHead == fHead)
+		{
+			return (1);
+		}
+	}
 
-    new->n = n;
-    new->next = *head;
-    *head = new;
-
-    return (new);
-}
-
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-    listint_t *current;
-
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
+	return (0);
 }
