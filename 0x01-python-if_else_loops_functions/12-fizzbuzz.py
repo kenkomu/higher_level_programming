58372238a41a80aaf91608323436a931257929d4
kenkomu <komukenneth216@gmail.com>
2022-07-02 09:18:35 +0000

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-for i in range(1,101):
-    if (i % 3 == 0):
-        print("Fizz", end = '')
-    elif (i % 5 == 0):
-        print("Buzz", end = '')
-    elif (i % 3 == 0, i % 5 == 0):
-        print("FizzBuzz", end = '')
+for i in range(1, 101):
+    if i % 15 == 0:
+        print("FizzBuzz", end='')
+    elif i % 5 == 0:
+        print("Buzz", end='')
+    elif i % 3 == 0:
+        print("Fizz", end='')
     else:
-        print(i, end = '')
-    print("", end = '')
+        print(i, end='')
+    print(" ", end='')
 print()
