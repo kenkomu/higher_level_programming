da445093e487fe0d809858ee9461f0e49a78a178
kenkomu <komukenneth216@gmail.com>
2022-07-04 09:22:40 +0300

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+from ast import operator
+from unittest import result
+
+
 if __name__ == "__main__":
     """
     Handle basic arithmetic operations.
@@ -8,10 +12,18 @@ if __name__ == "__main__":
     if len(sys.argv) -1 !=3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    ops = {"+": add,"-": sub,"*": mul, "/":div}
-    if sys.argv[2] not in list (ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print(f"{a} {sys.argv[2]} {b} = {ops[sys.argv[2]](a,b)}")
+    operator = sys.argv[2]
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a,b)
+    elif operator == '/':
+        result = mul(a/b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    print(f"{a} {operator} {b} = {result}")
