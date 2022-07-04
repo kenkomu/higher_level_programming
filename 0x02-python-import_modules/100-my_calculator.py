c8ee788168519b605762a7e7bcc98c7355097b8f
kenkomu <komukenneth216@gmail.com>
2022-07-04 09:14:45 +0300

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """
+    Handle basic arithmetic operations.
+    """
     from calculator_1 import add, sub, mul, div
     import sys
     if len(sys.argv) -1 !=3:
