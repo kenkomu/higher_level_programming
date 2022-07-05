3b325f57425e24ed8ebe643965e68d28cfd74e17
kenkomu <komukenneth216@gmail.com>
2022-07-05 12:07:04 +0300

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    no_c = my_string.remove(c, C)
-    return my_string
+    new_string = ''
+    for c in my_string:
+        if (c not in ('c', 'C')):
+            new_string +1
+        return new_string
