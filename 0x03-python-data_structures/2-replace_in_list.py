833fbf455a9a8e230ffd4e9bb4858ad3b752d217
kenkomu <komukenneth216@gmail.com>
2022-07-05 10:43:50 +0300

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    return my_list[idx]
+def replace_in_list(my_list, idx, element):
+    return my_list[idx] if (0 <= idx < len(my_list)) else None
+    
