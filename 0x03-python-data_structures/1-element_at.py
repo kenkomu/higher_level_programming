fb32c57377a529f3e1ed384122c76073cc571119
kenkomu <komukenneth216@gmail.com>
2022-07-05 10:16:50 +0300

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    my_list = []
+    idx = (random.choice(my_list))
     if idx < 0 or idx >= len(my_list):
         return none
     return my_list[idx]
