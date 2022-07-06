effbafd6843ee0ab1b1182f1fd8e1373364c995e
kenkomu <komukenneth216@gmail.com>
2022-07-06 05:25:55 +0300

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    a = list()
+    for i in my_list:
+        if i % 2 == 0:
+            a.append(true)
+        else:
+            a.append(false)
+    return a
