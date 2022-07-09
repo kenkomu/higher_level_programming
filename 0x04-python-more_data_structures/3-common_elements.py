b6669862599f8b71a00844fed18c1a42deeb90f5
kenkomu <komukenneth216@gmail.com>
2022-07-09 10:03:51 +0300

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set_1 = {}
-    set_2 = {}
-    common_elements = list(set(set_1). intersection(set_2))
-    return common_elements
+    set_1 = {"Python", "C", "Javascript"}
+    set_2 = {"Bash", "C", "Ruby", "Perl"}
+    c_set = common_elements(set_1, set_2)
+    print(sorted(list(c_set)))
