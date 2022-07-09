dc88d67650229d2736ef6b211d6b664b7b609568
kenkomu <komukenneth216@gmail.com>
2022-07-09 09:59:49 +0300

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,5 +2,5 @@
 def common_elements(set_1, set_2):
     set_1 = {}
     set_2 = {}
-    common_elements = set_1.intersection(set_2)
+    common_elements (set_1.intersection(set_2))
     return common_elements
