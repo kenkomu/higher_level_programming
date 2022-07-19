ec55bdfc3696dde35c5d94b9a4ff28109f656f54
kenkomu <komukenneth216@gmail.com>
2022-07-19 11:42:23 +0300

    prints the number of elements in a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for value in my_list[:x]:
         try:
-            print("{:d}".format(value), end = '')
+            print("{:d}".format(value), end='')
             count += 1
         except ValueError:
             pass
-        print()
+    print()
     return value
