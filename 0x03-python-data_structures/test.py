397a534f6071aaf1a56ddc4f9e60c958df139434
kenkomu <komukenneth216@gmail.com>
2022-07-19 11:36:49 +0300

    prints the number of elements of a list

---
+++ 0x03-python-data_structures/test.py
@@ -0,0 +1,7 @@
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for column in row:
+            print("{:d}".format(column), sep='', end='')
+            if (column != row[len(row) - 1]):
+                print(' ', end='')
+        print()
