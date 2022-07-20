c371931e4dfa36d8b424163aab972f642d77f63a
kenkomu <komukenneth216@gmail.com>
2022-07-20 22:53:07 +0300

    5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+from traceback import print_tb
+
+
+class Square:
+    """empty class Square that defines a square
+    Attributes
+
+    size: size of the square
+    """
+    __size = 0
+
+    def __init__(self, ssize=0):
+        self.__size = ssize
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, ssize=0):
+        if not isinstance(ssize, int):
+            raise TypeError("size must be an integer")
+        if ssize < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = ssize
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        for y in range(self.size):
+            [print("#", end="") for x in range(self.size)]
+            print()
+        if self.size == 0:
+            print()
