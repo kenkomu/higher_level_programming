8ae388c411ec22467a0d8e9027f3cce974000616
kenkomu <komukenneth216@gmail.com>
2022-07-20 22:24:05 +0300

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """empty class Square that defines a square
+    Attributes
+
+    size: size of the square
+    """
+    __size = 0
+
+    def __init__(self, ssize=0):
+        if not isinstance(int):
+            raise TypeError("size must be an integer")
+        if ssize < 0:
+            raise ValueError("size must be >= 0")
