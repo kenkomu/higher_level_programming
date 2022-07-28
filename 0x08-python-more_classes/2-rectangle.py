cc413fdebc875582567a6e79a83ca7ee702f6b25
kenkomu <komukenneth216@gmail.com>
2022-07-28 11:50:45 +0300

    2-rectangle.py

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+
+
+class Rectangle:
+    """empty class Rectangle that defines a rectangle
+    Attributes:
+        width: width of the rectangle
+        height: height of the rectangle
+    """
+    __width = 0
+    __height = 0
+
+    def __init__(self, wwidth=0, hheight=0):
+        self.width = wwidth
+        self.height = hheight
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, wwidth):
+        if not isinstance(wwidth, int):
+            raise TypeError("width must be an integer")
+        if wwidth < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = wwidth
+
+    @property
+    def height(self):
+        return self.__height
+
+
+    @height.setter
+    def height(self, hheight):
+        if not isinstance(hheight, int):
+            raise TypeError("height must be an integer")
+        if hheight < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = hheight
+
+    def area(self):
+        return (__height * __width)
