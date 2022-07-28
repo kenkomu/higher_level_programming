f6ba770a3a50f9ed6dada42ef5ed3382221bd55d
kenkomu <komukenneth216@gmail.com>
2022-07-28 11:10:30 +0300

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,6 +2,9 @@
 """Rectangle class"""
 
 
+from numpy import isin
+
+
 class Rectangle:
     """empty class Rectangle that defines a rectangle
     Attributes:
@@ -10,11 +13,29 @@ class Rectangle:
     """
     __width = 0
     __height = 0
-    def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+
+    def __init__(self, wwidth=0, hheight=0):
+        self.width = wwidth
+        self.height = hheight
 
     @property
     def width(self):
         return self.__width
 
+    @width.setter
+    def width(self, wwidth):
+        if not isinstance(wwidth, int):
+            raise TypeError("width must be an integer")
+        if wwidth < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, hheight):
+        if not isinstance(hheight, int):
+            raise TypeError("height must be an integer")
+        if hheight < 0:
+            raise ValueError("height must be >= 0")
