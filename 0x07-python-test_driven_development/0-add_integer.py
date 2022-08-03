cf40ccea344a6c86cc48945e6e72a79c844641fb
kenkomu <komukenneth216@gmail.com>
2022-08-03 08:14:51 +0300

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""add_integer module"""
+
 def add_integer(a, b=98):
+    """ add_integer function
+    this function add two integer
+    Attributes:
+        prmA: first integer
+        prmB: second integer
+    """
     if isinstance(a, float):
         a = int(a)
         raise TypeError
