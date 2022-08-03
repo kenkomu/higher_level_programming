6341353fdd6d21583735afa6a5ce0fc6029a6715
kenkomu <komukenneth216@gmail.com>
2022-08-03 11:06:35 +0300

    5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""text_indentation module"""
+
+
+def text_indentation(text):
+    """text_indentation function
+
+    prints a text with 2 new lines
+    Attributes:
+        text = the text
+    """
+    if isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    prmText = prmText.strip()
+    prmText = prmText.replace(".", ".\n")
+    prmText = prmText.replace("?", "?\n")
+    prmText = prmText.replace(":", ":\n")
+    prmText = prmText.rstrip()
+
+    if len(prmText) == 0:
+        return
+
+    for key, line in enumerate(list(prmText.split("\n"))):
+        line = line.strip()
+        print("{}".format(line))
+        if key < len(list(prmText.split("\n"))) - 1:
+            print()
