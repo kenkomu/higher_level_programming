32fe1c75db698d7f41c7371e979d47e4567e826f
kenkomu <komukenneth216@gmail.com>
2022-08-03 09:22:25 +0300

    3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""say_my_name module"""
+
+def say_my_name(first_name="kenneth", last_name="njoro"):
+    if not isinstance(first_name, str):
+        raise TypeError (
+            "first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError (
+            "last_name must be a string")
+    print("My name is {:d} {:d}",first_name,last_name)
