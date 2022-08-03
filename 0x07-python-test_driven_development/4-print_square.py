ee531d259b66aa20a49903f34a635b9b2c520045
kenkomu <komukenneth216@gmail.com>
2022-08-03 10:47:27 +0300

    4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""print_square module"""
+
+
+def print_square(size):
+    """print_square function
+
+    prints a square with the character #.
+
+    Attributes:
+        size = square size
+    """
+    if not isinstance(size, int) or isinstance(size, float):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for y in range(size):
+        [print("#", end='') for x in range(size)]
+        print()
