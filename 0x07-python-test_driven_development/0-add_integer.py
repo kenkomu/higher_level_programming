9bfd1f36acd10cbfda3d9cde87468bc1657a70b9
kenkomu <komukenneth216@gmail.com>
2022-08-03 08:25:08 +0300

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,9 +14,7 @@ def add_integer(a, b=98):
     if isinstance(b, float):
         b = int(b)
     if not isinstance(a, int):
-        raise TypeError
-        print("a must be an integer")
+        raise TypeError("a must be an integer")
     if not isinstance(b, int):
-        raise TypeError
-        print("b must be an integer")
+        raise TypeError("b must be an integer")
     return a + b
