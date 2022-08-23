eb9dfc3399106433d1ed2ea0f93bae0489e0ac3e
kenkomu <komukenneth216@gmail.com>
2022-08-23 22:06:50 +0300

    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -20,4 +20,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """ Area function """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return "[Square] {}/{}".format(self.__size, self.__size)
