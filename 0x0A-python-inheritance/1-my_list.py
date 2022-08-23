560c28dcc8deba3f8ff93dcdda94b8f0c99144b1
kenkomu <komukenneth216@gmail.com>
2022-08-23 14:41:46 +0300

    MyList

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" MyList module """
+
+
+class MyList:
+    """ MyList class """
+    def print_sorted(self):
+        """print_sorted function"""
+        print(sorted(MyList))
