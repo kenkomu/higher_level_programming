bdd62759a162000f125cee39ef380a7d4f0f99b1
kenkomu <komukenneth216@gmail.com>
2022-08-23 15:46:13 +0300

    4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""inherits_from module"""
+""" inherits_from module """
 
 
 def inherits_from(obj, a_class):
-    """inherits_from function"""
-    return (type(obj) is not a_class and
-            issubclass(type(obj, a_class)))
+    """ inherits_from function """
+    return (
+        type(obj) is not a_class and
+        issubclass(type(obj), a_class)
+    )
