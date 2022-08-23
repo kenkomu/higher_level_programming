5401ca3e84d8521d979682fd75cb70fcf17911e0
kenkomu <komukenneth216@gmail.com>
2022-08-23 21:11:11 +0300

    10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" Square module """
+
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
