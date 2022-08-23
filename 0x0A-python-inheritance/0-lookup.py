9bfabab7823bf2c9c85ffe66f0aeebee3cc59906
kenkomu <komukenneth216@gmail.com>
2022-08-23 13:42:15 +0300

    returns the list of available attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
-def lookup(obj):
-    obj.lookup
+class Number :
+      
+    # Class Attributes
+    one = 'first'
+    two = 'second'
+    three = 'third'
+      
+    def __init__(self, attr):
+        self.attr = attr
+          
+    def lookup(obj): 
+        print(obj.one, obj.two, obj.three, obj.attr)
+          
+n = Number(2)
+n.lookup()
