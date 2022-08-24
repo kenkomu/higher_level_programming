589373fc491272ef03a0eed1edab4507d400c9cb
kenkomu <komukenneth216@gmail.com>
2022-08-24 12:36:02 +0300

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ def read_file(filename="UTF8"):
     """read_file function"""
     with open(filename="UTF8", mode="r", encoding="utf-8") as myFile:
         read_file = myFile.read()
-
+        print(read_file)
     myFile.closed
     return read_file
