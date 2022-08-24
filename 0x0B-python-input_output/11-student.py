96dea668564acfe903978dc595636a576885a73a
kenkomu <komukenneth216@gmail.com>
2022-08-24 18:55:14 +0300

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""student module"""
+
+
+class Student:
+    """Student class"""
+    first_name = ""
+    last_name = ""
+    age = 0
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation function"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+            Function generate a json from that class
+        """
+        json = {}
+
+        if None is attrs:
+            return self.__dict__
+
+        for attr in attrs:
+            if type(attr) is not str:
+                continue
+            if hasattr(self, attr):
+                json[attr] = getattr(self, attr)
+
+        return json
+
+    def reload_from_json(self, json):
+        """
+            Function that force to load a json for an object
+        """
+        for key in json.keys():
+            self.__dict__[key] = json[key]
