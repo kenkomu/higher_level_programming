e203b4c513f197082c7cc0e8966ade1dd0d998a7
kenkomu <komukenneth216@gmail.com>
2022-08-24 12:31:20 +0300

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""read_file module"""
+
+
+def read_file(filename="UTF8"):
+    """read_file function"""
+    with open(filename="UTF8", mode="r", encoding="utf-8") as myFile:
+        read_file = myFile.read()
+
+    return read_file
