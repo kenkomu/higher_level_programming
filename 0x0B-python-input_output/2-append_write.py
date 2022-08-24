4e3693e5aa5433ab4b12fa43643651f17e92cb41
kenkomu <komukenneth216@gmail.com>
2022-08-24 13:16:20 +0300

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,10 +7,10 @@ def append_write(filename="", text=""):
         function that append a specific text in a specific file
         Args:
             fileName: name of the file
-            text: text to write
+            text: text to append
     """
     charCount = 0
-    with open(filename, mode="w", encoding="UTF-8") as myFile:
+    with open(filename, mode="a", encoding="UTF-8") as myFile:
         charCount = myFile.write(text)
     myFile.closed
     return charCount
