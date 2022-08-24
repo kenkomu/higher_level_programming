e728a766a443c4aa573d7292cd117f9919bc1a7f
kenkomu <komukenneth216@gmail.com>
2022-08-24 13:06:05 +0300

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,12 @@
 
 
 def write_file(filename="", text=""):
+    """
+        function that write a specific text in a specific file
+        Args:
+            fileName: name of the file
+            text: text to write
+    """
     charCount = 0
     with open(filename, mode="w", encoding="UTF-8") as myFile:
         charCount = myFile.write(text)
