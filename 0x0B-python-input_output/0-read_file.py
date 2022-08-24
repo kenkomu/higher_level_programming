f309f93ee7c21df03804a49144bc7a25b87f766b
kenkomu <komukenneth216@gmail.com>
2022-08-24 12:39:51 +0300

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,8 +2,12 @@
 """read_file module"""
 
 
-def read_file(filename="UTF8"):
-    """read_file function"""
+def read_file(filename=""):
+    """
+        function that read specific file
+        Args:
+            prmFileName: name of the file
+    """
     with open(filename="UTF8", mode="r", encoding="utf-8") as myFile:
         read_file = myFile.read()
         print(read_file, end="")
