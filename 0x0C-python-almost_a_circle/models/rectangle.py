9f058c5c2c5e09e54a1ee95ad8e3528c86432c7b
kenkomu <komukenneth216@gmail.com>
2022-08-27 11:32:09 +0300

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -86,5 +86,5 @@ class Rectangle(Base):
             print(" " * self.x + "#" * self.width)
 
     def __str__(self):
-        return ([Rectangle]({}) {}/{} - {}/{}.format(self.id, self.x,
-                self.y, self.width, self.height))
+        return [Rectangle] ({}) {}/{} - {}/{}.format(self.id, self.x,
+                self.y, self.width, self.height)
