c9a1efe41051c0673a77068a971a8fc1ce6757de
kenkomu <komukenneth216@gmail.com>
2022-09-08 13:36:06 +0300

    3-list_tables.sql

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,2 @@
--- lists all the tables of a database in your MySQL server.
-SELECT Table_name as TablesName from information_schema.tables where table_schema = 'mysql';
+-- lists all the tables of a database in MySQL server
+SHOW TABLES;
