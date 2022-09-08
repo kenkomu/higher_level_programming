d1e67a71850a4cceecc9ef78a04d3dcb17f177e9
kenkomu <komukenneth216@gmail.com>
2022-09-08 13:04:06 +0300

    0-list_databases.sql

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,2 +1,3 @@
-/*script that lists all databases of your MySQL server.
-SHOW DATABASES;
+/*script that lists all databases of your MySQL server*/
+SELECT schema_name 
+FROM information_schema.schemata;
