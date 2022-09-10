4b828f05897471352428a46fc02548d794e55718
kenkomu <komukenneth216@gmail.com>
2022-09-10 12:37:34 +0300

    10-genre_id_by_show.sql

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT s.title, sg.genre_id
+FROM tv_shows s
+INNER JOIN tv_show_genres sg ON s.id = sg.show_id
+ORDER BY s.title ASC, sg.genre_id ASC;
