d831734f0c800c6d84336f96689b7626cd970d7c
kenkomu <komukenneth216@gmail.com>
2022-09-10 12:21:23 +0300

    9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
--- lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM cities, states INNER JOIN states s ON c.state_id = s.id ORDER BY c.id ASC;
+--- lists all cities contained in the database hbtn_0d_usa.
+SELECT c.id, c.name, s.name FROM cities c INNER JOIN states s ON c.state_id = s.id ORDER BY c.id ASC;
