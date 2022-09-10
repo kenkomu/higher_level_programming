c66852926811b93e66409e9c075a86b944490bc6
kenkomu <komukenneth216@gmail.com>
2022-09-10 12:57:25 +0300

    15-comedy_only.sql

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT s.title
+FROM tv_shows s
+LEFT JOIN tv_show_genres sg ON sg.show_id = s.id
+LEFT JOIN tv_genres g ON sg.genre_id = g.id
+WHERE g.name = 'Comedy'
+ORDER BY s.title ASC, g.id ASC;
