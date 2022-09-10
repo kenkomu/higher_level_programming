e562299ed615166825bf4957e2be79d2f4274db4
kenkomu <komukenneth216@gmail.com>
2022-09-10 12:45:17 +0300

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,6 @@
 -- lists all shows contained in hbtn_0d_tvshows without a genre linked.SELECT s.title, sg.genre_id
+SELECT s.title, sg.genre_id
 FROM tv_shows s
-FULL OUTER JOIN tv_show_genres sg ON s.id = sg.show_id
+LEFT JOIN tv_show_genres sg ON sg.show_id = s.id
+WHERE sg.genre_id IS NULL
 ORDER BY s.title ASC, sg.genre_id ASC;
