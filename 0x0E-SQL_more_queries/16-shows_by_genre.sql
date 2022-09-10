4476b70eb775f8cd3482fe5b4d23b0226e410c44
kenkomu <komukenneth216@gmail.com>
2022-09-10 12:58:37 +0300

    16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+SELECT s.title, g.name
+FROM tv_shows s
+LEFT JOIN tv_show_genres sg ON sg.show_id = s.id
+LEFT JOIN tv_genres g ON sg.genre_id = g.id
+ORDER BY s.title ASC, g.name ASC;
