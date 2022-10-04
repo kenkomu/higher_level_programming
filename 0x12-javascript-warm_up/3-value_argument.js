fa667080da359be957fde5a76d1e2a63970ee615
kenkomu <komukenneth216@gmail.com>
2022-10-04 20:33:02 +0300

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 /**
  * prints the first argument passed to it:
  */
-if (process.argv === 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(process.argv[3]);
