02a95fa86334e023f21ac2227f20bb1449a3e69f
kenkomu <komukenneth216@gmail.com>
2022-10-04 22:12:10 +0300

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
 function add (a, b) {
-  let result;
-  if (isNaN(parseInt(process.argv[2] || parseInt(process.argv[3])))) {
-    console.log();
-  } else {
-    result = a + b;
-    console.log(result);
-  }
+  return (a + b);
 }
-add(process.argv[2], process.argv[3]);
+console.log(add(parseInt(process.argv[2])), (parseInt(process.argv[3])));
