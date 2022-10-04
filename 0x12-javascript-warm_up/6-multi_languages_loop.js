c4e6bf798836c26d448e58eff6a3a2c6ca80c00d
kenkomu <komukenneth216@gmail.com>
2022-10-04 21:35:08 +0300

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
+/**
+ * prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+ */
 const message = Array['C is fun' + 'Python is cool' + 'JavaScript is amazing'];
 message.foreach(element => (console.log(element)));
