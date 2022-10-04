cb7ea2952cdd6ad9e8b79dd1ecd9f28a4d5c3ff2
kenkomu <komukenneth216@gmail.com>
2022-10-04 21:08:37 +0300

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
-console.log('My number:', process.argv[2]);
+if (isNaN(parseInt(process.argv[2]))) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', (parseInt(process.argv[2])));
+}
