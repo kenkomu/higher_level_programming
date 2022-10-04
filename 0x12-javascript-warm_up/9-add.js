65626b00141616d67ce1459e8afda32d2940cc01
kenkomu <komukenneth216@gmail.com>
2022-10-04 22:07:23 +0300

    9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function add (a, b) {
+  let result;
+  if (isNaN(parseInt(process.argv[2] || parseInt(process.argv[3])))) {
+    console.log('NaN');
+  } else {
+    result = a + b;
+    console.log(result);
+  }
+}
+add(process.argv[2], process.argv[3]);
