fda581b313a6971f7aba226bab01a0897b9d3e12
kenkomu <komukenneth216@gmail.com>
2022-10-04 21:37:56 +0300

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,6 +3,7 @@
 /**
  * prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
  */
-const message = Array['C is fun' + 'Python is cool' + 'JavaScript is amazing'];
 
-message.foreach(element => (console.log(element)));
+const sentences = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+sentences.forEach(element => console.log(element));
