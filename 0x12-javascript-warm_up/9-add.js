74a03708001f4c0c1585c5085fd4f98d7158cf85
kenkomu <komukenneth216@gmail.com>
2022-10-04 22:13:12 +0300

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
+/**
+ * Add two numbers
+ *
+ * @param {*} a first number
+ * @param {*} b second number
+ *
+ * @returns result of addition of two parameters
+ */
 function add (a, b) {
   return (a + b);
 }
+/**
+ * prints the addition of 2 integers
+ */
 console.log(add(parseInt(process.argv[2])), (parseInt(process.argv[3])));
