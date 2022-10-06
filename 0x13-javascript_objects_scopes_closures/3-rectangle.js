fe4571889a4504c1e158223a60a84e4911797844
kenkomu <komukenneth216@gmail.com>
2022-10-06 22:45:31 +0300

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let row = 0; row < this.height; row++) {
+      for (let column = 0; column < this.width; column++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('/n');
+    }
+  }
+};
