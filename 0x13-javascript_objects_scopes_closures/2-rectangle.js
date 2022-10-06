e29680ed19a33ef74ec19b18a97225b75f7b5692
kenkomu <komukenneth216@gmail.com>
2022-10-06 21:09:29 +0300

    2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+
+    if (width === 0 || height === 0) {
+      const objectB = new Object();
+    }
+  }
+}
+module.exports = Rectangle;
