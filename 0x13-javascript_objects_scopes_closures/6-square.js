e742328f25f5c6378c26787b676c1dd50a32a5eb
kenkomu <komukenneth216@gmail.com>
2022-10-08 23:09:02 +0300

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,17 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square');
 
 module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
   charPrint (c) {
-    for (let row = 0; row < this.size; row++) {
-      for (let column = 0; column < this.size; column++) {
-        process.stdout.write('c');
-        if (c === undefined);
-        process.stdout.write('X');
+    if (c === undefined) {
+      process.stdout.write('');
+    } else {
+      for (let row = 0; row < this.height; row++) {
+        for (let column = 0; column < this.width; column++) {
+          process.stdout.write('X');
+        }
+        process.stdout.write('\n');
       }
-      process.stdout.write('\n');
     }
   }
 };
