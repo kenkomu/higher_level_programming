c6fff02073679fccbe48d60b4829f7992a0186bc
kenkomu <komukenneth216@gmail.com>
2022-10-08 22:22:23 +0300

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let row = 0; row < this.height; row++) {
+      for (let column = 0; column < this.width; column++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+
+  rotate () {
+    return this.width === this.height;
+  }
+
+  double () {
+    return (this.width * 2, this.height * 2);
+  }
+};
