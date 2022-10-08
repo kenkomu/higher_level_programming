5f7d32e1ec11358c0784f232e13f093cbae495f8
kenkomu <komukenneth216@gmail.com>
2022-10-08 22:57:08 +0300

    5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,7 +2,7 @@
 const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
-  constructor (width, height, size) {
-    super(width, height);
+  constructor (size) {
+    super(size, size);
   }
 };
