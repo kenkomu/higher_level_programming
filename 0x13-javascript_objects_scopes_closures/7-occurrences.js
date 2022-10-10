1e539dd4cd6781ad3fbc441f439d9b395fb024b9
kenkomu <komukenneth216@gmail.com>
2022-10-10 06:58:54 +0300

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  this.list = [5, 5, 5, 2, 2, 2, 2, 2, 9, 4];
-  this.searchElement = {};
-
-  for (const element of list) {
-    if (searchElement[element]) {
-      searchElement[element] += 1;
-    } else {
-      searchElement[element] = 1;
+  let count = 0;
+  list.forEach(function (element) {
+    if (element === searchElement) {
+      count++;
     }
-  }
+  });
+  return count;
 };
