f85fe511fef2dd869a64ba0498b1a3cba1c2d1e3
kenkomu <komukenneth216@gmail.com>
2022-10-10 08:08:56 +0300

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const { dict } = require('101-data.js');
+const { dict } = require('101-data');
 const newDict = {};
 for (const [, value] of Object.entries(dict)) {
   const list = [];
