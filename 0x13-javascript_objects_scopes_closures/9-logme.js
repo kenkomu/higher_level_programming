be334e89712d5967ac48bf955309ee2150f5628a
kenkomu <komukenneth216@gmail.com>
2022-10-10 07:15:43 +0300

    9-logme.js

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let argumentnumber = 0;
 exports.logMe = function (item) {
-  console.log(argumentnumber + ':' + item);
+  console.log(argumentnumber + ': ' + item);
   argumentnumber++;
 };
