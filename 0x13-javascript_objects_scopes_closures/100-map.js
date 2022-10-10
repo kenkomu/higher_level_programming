70e611e73845e21eae64d849ed2420d144730f6b
kenkomu <komukenneth216@gmail.com>
2022-10-10 07:59:38 +0300

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 const { list } = require('./100-map');
 const map1 = list.map((Element, index) => Element * index);
-console.log(list, map1);
+
+console.log(list);
+console.log(map1);
