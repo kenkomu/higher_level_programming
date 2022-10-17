81305af86aee49ff1fbe376a45f9a3bf3d9fa991
kenkomu <komukenneth216@gmail.com>
2022-10-17 11:33:11 +0300

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+    script that lists all states from the database hbtn_0e_0_usa
+"""
+
 import MySQLdb
 conn = MySQLdb.connect(user="root", passwd="root", db="hbtn_0e_0_usa")
 cur = conn.cursor()
